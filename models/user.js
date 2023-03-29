2d46b53b25ced30a837b99dbb7c979b979bd69b8
weeklies <80141759+weeklies@users.noreply.github.com>
2023-03-29 18:00:22 +0100

    More forms

--- models/user.js
+++ models/user.js
@@ -3,9 +3,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+    // Not using first and last name as it may not be inclusive.
+    fullname: { type: String, required: true },
     username: { type: String, required: true },
-    // This is hashed using bcrypt.
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+
+    member: { type: Boolean, required: true },
+    admin: { type: Boolean, required: false }
 });
 
 module.exports = mongoose.model("User", UserSchema);
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const bcrypt = require("bcryptjs");
 const passport = require("passport");
 
+// TODO migrate logic to controller.
 const user_controller = require("../controllers/userController");
 const message_controller = require("../controllers/messageController");
 
@@ -11,7 +12,7 @@ const User = require("../models/user");
 const Message = require("../models/message");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get(['/', '/log-in'], function (req, res, next) {
   res.render("index", { title: "Members Only", user: req.user },);
 });
 
@@ -33,12 +34,23 @@ router.post(
 
 
 router.get("/sign-up", (req, res) => {
+
   return res.render("sign-up-form");
 });
 
 router.post("/sign-up", async (req, res, next) => {
+  // Check if user already exists.
+  const user = await User.findOne({ username: req.body.username });
+  if (user) {
+    err = new Error('Account already exists.');
+    return next(err);
+  }
+
+  // TODO Validate password length and password match.
+
   bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
     if (err) return next(err);
+
     const user = new User({
       username: req.body.username,
       password: hashedPassword,
@@ -48,5 +60,11 @@ router.post("/sign-up", async (req, res, next) => {
   }
   );
 });
+
+router.get('/membership', (req, res) => {
+  if (!req.user) return res.redirect('/log-in');
+
+  return res.render("membership");
+})
 
 module.exports = router;
