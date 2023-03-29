54cadadb984a503df0271b8886e67ee3349d68ec
weeklies <80141759+weeklies@users.noreply.github.com>
2023-03-29 14:44:21 +0100

    Use bcrypt

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+
+const bcrypt = require("bcryptjs");
+var createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
@@ -25,13 +28,83 @@ app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, message, { message: "Incorrect username" });
+      }
+
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          return done(null, user)
+        } else {
+          return done(null, false, { message: "Incorrect password" })
+        }
+      })
+    }
+    catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => res.render("index", { title: "Hhh" }));
+app.get("/", (req, res) => res.render("index", { title: "Title", user: req.user },));
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
+
+app.get("/log-out", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
+
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+app.post("/sign-up", async (req, res, next) => {
+  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+    if (err) return next(err);
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.redirect("/");
+  }
+  );
+});
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
